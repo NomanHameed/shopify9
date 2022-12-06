b86a349d782e47ef0e45f592b9bd97334845a559
Noman Hameed <nomanbutt8322@gmail.com>
2022-12-06 16:35:18 +0500

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <!-- You are: (shop domain name) -->
     <p>You are: {{ $shopDomain ?? Auth::user()->name }}</p>
     <h1>This is from Laravel Application</h1>
+
 @endsection
 
 @section('scripts')
