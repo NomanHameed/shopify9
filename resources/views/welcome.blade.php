e1d92027ab01ad82c6e281fb34c4c7cbcd477a28
Noman Hameed <nomanbutt8322@gmail.com>
2022-12-07 12:18:40 +0500

    add test route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <!-- You are: (shop domain name) -->
     <p>You are: {{ $shopDomain ?? Auth::user()->name }}</p>
+    @dd(auth()->user());
     <h1>This is from Laravel Application</h1>
 
 @endsection
