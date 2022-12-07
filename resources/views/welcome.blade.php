c53253f86e16ad175ebeae1976e52a7a23f1b1e1
Noman Hameed <nomanbutt8322@gmail.com>
2022-12-07 13:59:04 +0500

    add dd in welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
     <!-- You are: (shop domain name) -->
     <p>You are: {{ $shopDomain ?? Auth::user()->name }}</p>
     <h1>This is from Laravel Application</h1>
-
+    {{ $res = auth()->user()->api()->rest('GET','/admin/api/2022-10/products.json') }}
+    {{ dd($res) }}
 @endsection
 
 @section('scripts')
