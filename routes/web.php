2a68c8971eedb05097808f5048f8bf02b132a0ce
Noman Hameed <nomanbutt8322@gmail.com>
2022-12-07 11:46:40 +0500

    solve exception

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->middleware(['verify.shopify'])->name('home');
+
+Route::get('/login', function (){
+    return view('login');
+});
+
