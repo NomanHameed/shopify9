63510873e62f6dd9520939d0279de102d2702896
Noman Hameed <nomanbutt8322@gmail.com>
2022-12-09 15:09:14 +0500

    post script file in shopify

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,27 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Hello World!");
-});
-// var xhr =  new XMLHttpRequest();
-// // xhr.open('GET', )
-//
-// xhr.onreadystatechange = function () {
-//     alert('script loaded');
-//     if(xhr.readyState === 4 && xhr.status === 200) {
-//         var response = JSON.parse(this.responseText);
-//         if(response.status) {
-//             function appendButton(elementClass, url){
-//                 var buttonEl = document.createElement("a");
-//                 buttonEl.href = url;
-//                 var buttonTextEl = document.createElement("span");
-//                 buttonTextEl.className = "picon-p-add-news";
-//                 buttonTextEl.innerText = "Read more news";
-//                 buttonEl.appendChild(buttonTextEl);
-//                 document.getElementsByClassName(elementClass).appendChild(buttonEl);
-//             }
-//             appendButton('product-form__buttons', 'https://www.google.com/');
-//
-//         }
-//     }
-// }
-// xhr.send();
-//
+var xhr =  new XMLHttpRequest();
+// xhr.open('GET', )
+
+xhr.onreadystatechange = function () {
+    if(xhr.readyState === 4 && xhr.status === 200) {
+        var response = JSON.parse(this.responseText);
+        if(response.status) {
+            console.log('asdfasdfasdfasdf');
+            // function appendButton(elementClass, url){
+            //     var buttonEl = document.createElement("a");
+            //     buttonEl.href = url;
+            //     var buttonTextEl = document.createElement("span");
+            //     buttonTextEl.className = "picon-p-add-news";
+            //     buttonTextEl.innerText = "Read more news";
+            //     buttonEl.appendChild(buttonTextEl);
+            //     document.getElementsByClassName(elementClass).appendChild(buttonEl);
+            // }
+            // appendButton('product-form__buttons', 'https://www.google.com/');
+
+        }
+    }
+}
+xhr.send();
+
