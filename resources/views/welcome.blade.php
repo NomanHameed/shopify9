a44326dde59756531b6bfa7ad9125087405e6682
Noman Hameed <nomanbutt8322@gmail.com>
2022-12-09 16:51:39 +0500

    add buttons to theme

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,11 @@
     <h2>Make an Offer Button Function</h2>
     <p>You are: {{ $shopDomain ?? Auth::user()->name }}</p>
 
-{{--    <div>--}}
-{{--        <div><button type="button" id="addOffer">Add Offer</button></div>--}}
-{{--        <div><button type="button" id="updateOffer">Update Offer</button></div>--}}
-{{--        <div><button type="button" id="deleteOffer">Delete Offer</button></div>--}}
-{{--    </div>--}}
+    <div>
+        <div><button class="btn--secondary" type="button" id="addOffer">Add Offer</button></div>
+        <div><button class="btn--secondary" type="button" id="updateOffer">Update Offer</button></div>
+        <div><button class="btn--secondary" type="button" id="deleteOffer">Delete Offer</button></div>
+    </div>
 {{--    @dd($res)--}}
 @endsection
 
@@ -18,6 +18,9 @@
 
     <script>
         actions.TitleBar.create(app, { title: 'Welcome' });
+        // document.getElementById('addOffer').onClick = function (){
+        //     alert('Add Button Alert');
+        // }
     </script>
 
 @endsection
