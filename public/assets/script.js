76fb47796c2a51f60909e4f9b40ca0d499d04f19
Noman Hameed <nomanbutt8322@gmail.com>
2022-12-09 15:14:09 +0500

    post script file in shopify

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,24 +1,34 @@
-var xhr =  new XMLHttpRequest();
-// xhr.open('GET', )
+var buttonEl = document.createElement("a");
+buttonEl.href = 'https://www.google.com/';
+var buttonTextEl = document.createElement("span");
+buttonTextEl.className = "picon-p-add-news";
+buttonTextEl.innerText = "Read more news";
+buttonEl.appendChild(buttonTextEl);
+document.getElementsByClassName('product-form__buttons').appendChild(buttonEl);
 
-xhr.onreadystatechange = function () {
-    if(xhr.readyState === 4 && xhr.status === 200) {
-        var response = JSON.parse(this.responseText);
-        if(response.status) {
-            console.log('asdfasdfasdfasdf');
-            // function appendButton(elementClass, url){
-            //     var buttonEl = document.createElement("a");
-            //     buttonEl.href = url;
-            //     var buttonTextEl = document.createElement("span");
-            //     buttonTextEl.className = "picon-p-add-news";
-            //     buttonTextEl.innerText = "Read more news";
-            //     buttonEl.appendChild(buttonTextEl);
-            //     document.getElementsByClassName(elementClass).appendChild(buttonEl);
-            // }
-            // appendButton('product-form__buttons', 'https://www.google.com/');
 
-        }
-    }
-}
-xhr.send();
 
+// var xhr =  new XMLHttpRequest();
+// // xhr.open('GET', )
+//
+// xhr.onreadystatechange = function () {
+//     if(xhr.readyState === 4 && xhr.status === 200) {
+//         var response = JSON.parse(this.responseText);
+//         if(response.status) {
+//             console.log('asdfasdfasdfasdf');
+//             // function appendButton(elementClass, url){
+//             //     var buttonEl = document.createElement("a");
+//             //     buttonEl.href = url;
+//             //     var buttonTextEl = document.createElement("span");
+//             //     buttonTextEl.className = "picon-p-add-news";
+//             //     buttonTextEl.innerText = "Read more news";
+//             //     buttonEl.appendChild(buttonTextEl);
+//             //     document.getElementsByClassName(elementClass).appendChild(buttonEl);
+//             // }
+//             // appendButton('product-form__buttons', 'https://www.google.com/');
+//
+//         }
+//     }
+// }
+// xhr.send();
+//
