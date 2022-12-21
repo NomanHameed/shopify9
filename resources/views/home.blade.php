04e919619ceb8f2733711769e814c91727b13775
Noman Hameed <nomanbutt8322@gmail.com>
2022-12-21 17:26:12 +0500

    offset

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
-@extends('shopify-app::layouts.default')
+{{--@extends('shopify-app::layouts.default')--}}
 
-@section('content')
+{{--@section('content')--}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <!-- You are: (shop domain name) -->
@@ -30,10 +30,10 @@
         </div>
 
 
-@endsection
+{{--@endsection--}}
 
-@section('scripts')
-    @parent
+{{--@section('scripts')--}}
+{{--    @parent--}}
 
     <script>
         actions.TitleBar.create(app, {title: 'Welcome'});
@@ -42,4 +42,4 @@
         }
     </script>
 
-@endsection
+{{--@endsection--}}
